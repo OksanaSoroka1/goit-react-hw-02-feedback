f256dd998cc70f69ad07e914b9fcb8aa763258a1
Oksana Soroka <oksanabodnaruk1507@gmail.com>
2021-12-08 22:18:50 +0200

    add changes

--- src/components/Notification/notification.js
+++ src/components/Notification/notification.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 const Notification = ({ message }) => {
   return <p>{message}</p>;
 };
+
 Notification.propTypes = {
   message: PropTypes.string,
 };
--- src/components/Statistics/statistics.js
+++ src/components/Statistics/statistics.js
@@ -1,17 +1,36 @@
 import PropTypes from 'prop-types';
+import css from './statistics.module.css';
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
   return (
-    <div>
-      <p>No feedback given</p>
-      <ul>
-        <li>Good: {good}</li>
-        <li>Neutral: {neutral}</li>
-        <li>Bad: {bad}</li>
-        <li>Total: {total}</li>
-        <li>Positive feedback: {positivePercentage} %</li>
-      </ul>
-    </div>
+    <ul className={css.list}>
+      <li className={css.item}>
+        <p className={css.text}>
+          Good:<span className={css.span}> {good}</span>
+        </p>
+      </li>
+      <li className={css.item}>
+        <p className={css.text}>
+          Neutral:<span className={css.span}>{neutral}</span>{' '}
+        </p>
+      </li>
+      <li className={css.item}>
+        <p className={css.text}>
+          Bad:<span className={css.span}>{bad}</span>
+        </p>{' '}
+      </li>
+      <li className={css.item}>
+        <p className={css.text}>
+          Total:<span className={css.span}>{total}</span>
+        </p>{' '}
+      </li>
+      <li className={css.item}>
+        <p className={css.text}>
+          Positive feedback:
+          <span className={css.span}> {positivePercentage}%</span>
+        </p>{' '}
+      </li>
+    </ul>
   );
 };
 Statistics.propTypes = {
